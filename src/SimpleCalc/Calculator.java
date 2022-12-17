dde1454117116bce2b7f9a1c18ea33c184deae29
CastroNicole <oblivionshikigami97@gmail.com>
2022-12-17 14:11:54 +0800

    Add Calculator Exception Handling

--- src/SimpleCalc/Calculator.java
+++ src/SimpleCalc/Calculator.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame{
     private JPanel panel1;
@@ -8,9 +10,42 @@ public class Calculator extends JFrame{
     private JTextField textField2;
     private JComboBox comboBox1;
     private JButton computeResultButton;
+    private JTextField textResult;
 
-    public Calculator(){
+public Calculator(){
+    computeResultButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try{
+              int num1 = Integer.parseInt(textField1.getText());
+              int num2 = Integer.parseInt(textField2.getText());
+              int result;
 
+              if(comboBox1.getSelectedItem().equals("+")){
+                  result = num1 + num2;
+                  String str = String.valueOf(result);
+                  textResult.setText(str);
+              }
+              if(comboBox1.getSelectedItem().equals("-")){
+                  result = num1 - num2;
+                  String str = String.valueOf(result);
+                  textResult.setText(str);
+              }
+                if(comboBox1.getSelectedItem().equals("*")){
+                  result = num1 * num2;
+                  String str = String.valueOf(result);
+                  textResult.setText(str);
+            }
+                if(comboBox1.getSelectedItem().equals("/")){
+                  result = num1 / num2;
+                  String str = String.valueOf(result);
+                  textResult.setText(str);
+              }
+            } catch(NumberFormatException a){
+                JOptionPane.showMessageDialog(null, "Invalid");
+            }
+        }
+    });
 }
 
     public static void main(String[] args) {
