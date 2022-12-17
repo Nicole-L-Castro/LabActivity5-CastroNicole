dea0fb4f577d7b01db3925abcdfe6da62ee63a47
CastroNicole <oblivionshikigami97@gmail.com>
2022-12-17 12:03:01 +0800

    Rename FoodOrderingSystem Components

--- src/SimpleCalc/Calculator.java
+++ src/SimpleCalc/Calculator.java
@@ -2,13 +2,14 @@ package SimpleCalc;
 
 import javax.swing.*;
 
-public class Calculator {
+public class Calculator extends JFrame{
     private JPanel panel1;
     private JTextField textField1;
+    private JTextField textField2;
     private JComboBox comboBox1;
     private JButton computeResultButton;
 
-public Calculator{
+    public Calculator(){
 
 }
 
