99b5aa0e4863554e1938071dcce18cd7f27ba2ee
CastroNicole <oblivionshikigami97@gmail.com>
2022-12-17 20:14:19 +0800

    Complete FoodOrderingSystem

--- src/FoodOrdering/FoodOrderingSystem.java
+++ src/FoodOrdering/FoodOrderingSystem.java
@@ -19,8 +19,39 @@ public class FoodOrderingSystem extends JFrame {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
-    public static void main(String[] args) {
+    public FoodOrderingSystem() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double totalPrice = 0;
+                    totalPrice += cPizza.isSelected() ? 100 : 0;
+                    totalPrice += cBurger.isSelected() ? 80 : 0;
+                    totalPrice += cFries.isSelected() ? 65 : 0;
+                    totalPrice += cSoftDrinks.isSelected() ? 55 : 0;
+                    totalPrice += cTea.isSelected() ? 50 : 0;
+                    totalPrice += cSundae.isSelected() ? 40 : 0;
+                    if (rb5.isSelected()) {
+                        totalPrice *= 0.95;
+                    } else if (rb10.isSelected()) {
+                        totalPrice *= 0.9;
+                    } else if (rb15.isSelected()) {
+                        totalPrice *= 0.85;
+                    }
+                    DecimalFormat df = new DecimalFormat("#.00");
+                    JOptionPane.showMessageDialog(null, "The total price is Php " + df.format(totalPrice));
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "An error occurred: " + ex.getMessage());
+                }
+            }
+        });
+    }
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Food Ordering System");
+        frame.setContentPane(new FoodOrderingSystem().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
-
