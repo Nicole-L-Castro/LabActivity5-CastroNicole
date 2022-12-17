06702b6fb628ad5de36e461a7514d4176bd7fd1a
CastroNicole <oblivionshikigami97@gmail.com>
2022-12-17 20:12:52 +0800

    Add FoodOrderingSystem main function

---
+++ src/FoodOrdering/FoodOrderingSystem.java
@@ -0,0 +1,26 @@
+package FoodOrdering;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+public class FoodOrderingSystem extends JFrame {
+    private JPanel panel1;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+    public static void main(String[] args) {
+
+    }
+}
+
